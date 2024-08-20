4be48d0ff41df4f6e8e1ecf2740b4cc76589c1bf
e-davies <emmi.lou002@gmail.com>
2024-08-20 20:04:04 +0100

    created 2 modals

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -1,16 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{title}}</h1>
+  <div v-if="showDefaultModal">
+    <Modal :header="header" :msg="msg" @close="toggleDefaultModal"/>
+  </div>
+  <div v-if="showSalesModal">
+    <Modal header="Sales Modal" msg="This is the message for the sales modal" theme="sale" @close="toggleSalesModal"/>
+  </div>
+  <button @click="toggleDefaultModal">Default Modal</button>
+  <button @click="toggleSalesModal">Sales Modal</button>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Modal
+  },
+  data(){
+    return{
+      title: 'My First Vue App',
+      header: 'Default Modal ', 
+      msg: 'This is the message for the default modal',
+      showDefaultModal: false,
+      showSalesModal: false
+    }
+  },
+  methods: {
+    toggleDefaultModal(){
+      this.showDefaultModal = !this.showDefaultModal
+    },
+    toggleSalesModal(){
+      this.showSalesModal = !this.showSalesModal
+    }
+  },
 }
 </script>
 
